5162e6cff75db5694d415d59ca3ca3bde31c1634
Thomas Hagebols <ThomasHagebols@users.noreply.github.com>
2021-05-23 21:27:06 +0200

    Fix

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -3,20 +3,20 @@ const github = require("@actions/github");
 
 async function run() {
     try {
-        const issueTitle = core.getInput("issue-title");
-        const jokeBody = core.getInput("joke");
-        const token = core.getInput("repo-token");
+        const issueTitle = core.getInput("issue-title")
+        const jokeBody = core.getInput("joke")
+        const token = core.getInput("repo-token")
 
-        const octokit = github.getOctokit(token);
+        const octoKit = new github.GitHub(token)
 
-        const newIssue = await octokit.issues.create({
+        const newIssue = await octoKit.issues.create({
             repo: github.context.repo.repo,
             owner: github.context.repo.owner,
             title: issueTitle,
             body: jokeBody
-        });
+        })
     } catch (err) {
-        core.setFailed(err.message);
+        core.setFailed(err.message)
     }
 }
 
